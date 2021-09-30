0f693d2544741dbdfddc3d69fcfc7941e7c586a7
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-30 12:49:58 +0530

    Added : find max value between three integers

--- src/com/bridgelab/FindMaxValue.java
+++ src/com/bridgelab/FindMaxValue.java
@@ -1,22 +1,13 @@
 package com.bridgelab;
 
-public class FindMaxValue<T extends Comparable<T>> {
-
-	T a1, a2, a3;
-
-	public FindMaxValue(T a1, T a2, T a3) {
-		this.a1 = a1;
-		this.a2 = a2;
-		this.a3 = a3;
-	}
-
+public class FindMaxValue {
 	/**
-	 * Generic method to compare any type of data and find maximum a1 : First value
-	 * to compare a2 : Second value to compare a3 : Third value to compare return
-	 * max : Maximum of three values
+	 * Method used to compare three integer values and get the maximum value. a1 :
+	 * First value to compare a2 : Second value to compare a3 : Third value to
+	 * compare return max : Maximum of three integers
 	 */
-	public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
-		T max = a1;
+	public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3) {
+		Integer max = a1;
 		if (a2.compareTo(max) > 0)
 			max = a2;
 		if (a3.compareTo(max) > 0)
@@ -26,12 +17,7 @@ public class FindMaxValue<T extends Comparable<T>> {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the program to find maximum value ");
-		Integer a1 = 30, a2 = 40, a3 = 90;
-		System.out.println("The Maximum between three integers is : " + maxOfValues(a1, a2, a3));
-		Float f1 = 3.5f, f2 = 9.4f, f3 = 7.1f;
-		System.out.println("The Maximum between three float is : " + maxOfValues(f1, f2, f3));
-		String s1 = "abc", s2 = "pqr", s3 = "xyz";
-		System.out.println("The Maximum between three String is : " + maxOfValues(s1, s2, s3));
+		Integer a1 = 3, a2 = 4, a3 = 9;
+		System.out.println("The Maximum between three integers is : " + maxOfInteger(a1, a2, a3));
 	}
-
 }
